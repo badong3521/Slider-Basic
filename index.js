80b6d7eb8adea0d361afea0dbd660d3b20b0a0b5
BaDong <dongba352001@gmail.com>
2022-05-24 21:11:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const btnPrev = $(".btn-prev");
+const btnNext = $(".btn-next");
+const imgCurrent = $(".current-img");
+const itemImgs = $$(".item-img");
+
+var currentIndex = 0;
+
+function updateImg(index) {
+  currentIndex = index;
+  itemImgs.forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  imgCurrent.src = itemImgs[index].getAttribute("src");
+  itemImgs[currentIndex].classList.add("active");
+}
+
+itemImgs.forEach((itemImg, index) => {
+  itemImg.addEventListener("click", (e) => {
+    updateImg(index);
+  });
+});
+
+btnNext.addEventListener("click", function () {
+  if (currentIndex == itemImgs.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  imgCurrent.style.animation = "sliderNext 0.5s ease-in-out ";
+  setTimeout(() => {
+    imgCurrent.style.animation = "";
+  }, 400);
+  updateImg(currentIndex);
+});
+
+btnPrev.addEventListener("click", function () {
+  if (currentIndex == 0) {
+    currentIndex = itemImgs.length - 1;
+  } else {
+    currentIndex--;
+  }
+  imgCurrent.style.animation = "sliderPrev 0.5s ease-in-out ";
+  setTimeout(() => {
+    imgCurrent.style.animation = "";
+  }, 400);
+  updateImg(currentIndex);
+});
